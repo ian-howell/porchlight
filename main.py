5d2b6821205bc8065a4de5ff0f9360d84039df19
Ian Howell <ian.howell0@gmail.com>
2018-12-31 01:20:57 -0600

    Add logging

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from configparser import ConfigParser
+import logging
+import os
 import requests
 import sys
 import time
@@ -7,6 +9,9 @@ import time
 PORCH_LIGHT = 4
 MINUTE = 60
 HOUR = 60 * MINUTE
+
+LOG = logging.getLogger()
+LOG.setLevel(logging.INFO)
 
 
 def main():
@@ -25,26 +30,33 @@ def main():
     weather_api_key = weather['api_key']
     city_id = weather['city_id']
 
+    setup_logging()
+
     light_on = False
     sundown = get_sundown(weather_api_key, city_id)
     while True:
         if light_on:
             if time.localtime(time.time()).tm_hour >= 22:
+                LOG.info("Turning off the lights")
                 turn_off_light(hue_ip, username, PORCH_LIGHT)
                 light_on = False
                 # It's 10PM, might as well sleep until 2PM tomorrow
                 time.sleep(16 * HOUR)
                 # By the time this executes, it's the next day
+                setup_logging()
                 sundown = get_sundown(weather_api_key, city_id)
             else:
                 # Waiting for 10PM
+                LOG.info("Waiting for 10PM")
                 time.sleep(5 * MINUTE)
         else:
             if time.time() >= sundown:
                 turn_on_light(hue_ip, username, PORCH_LIGHT)
+                LOG.info("Turning on the lights")
                 light_on = True
             else:
                 # Waiting for sundown
+                LOG.info("Waiting for sundown")
                 time.sleep(5 * MINUTE)
 
 
@@ -72,6 +84,21 @@ def _build_url(key, city):
     app_id = "APPID={}".format(key)
     base_url = "http://api.openweathermap.org/data/2.5/weather"
     return "{}?{}&{}".format(base_url, city_id, app_id)
+
+
+def setup_logging():
+    cwd = os.path.dirname(os.path.realpath(__file__))
+    date = time.strftime("%Y-%m-%d", time.localtime(time.time()))
+    log_file = "{}/logs/{}.log".format(cwd, date)
+
+    fileh = logging.FileHandler(log_file, 'a')
+    fmt = "%(asctime)s - %(message)s"
+    formatter = logging.Formatter(fmt, datefmt="%H:%M:%S")
+    fileh.setFormatter(formatter)
+
+    for hdlr in LOG.handlers[:]:
+        LOG.removeHandler(hdlr)
+    LOG.addHandler(fileh)
 
 
 if __name__ == "__main__":
